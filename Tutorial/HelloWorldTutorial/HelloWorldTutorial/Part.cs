84f13bc7e023a79a3c5442bd744bca4c5cacec70
Milan L Hazra <Milan.Hazra@in.ey.com>
2022-12-29 11:51:01 +0530

    List

---
+++ Tutorial/HelloWorldTutorial/HelloWorldTutorial/Part.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTutorial
+{
+    public class Part
+    {
+        public string PartName { get; set; }
+
+        public int PartId { get; set; }
+    }
+}
--- Tutorial/HelloWorldTutorial/HelloWorldTutorial/Program.cs
+++ Tutorial/HelloWorldTutorial/HelloWorldTutorial/Program.cs
@@ -116,6 +116,20 @@ var sb = new StringBuilder() ;
 var testStringBuilder = sb.Append(firstName) ;
 
 
+// List in C#
+List<Part> lst = new List<Part>();
+lst.Add(new Part { PartName = "Headlight", PartId = 1 });
+lst.Add(new Part { PartName = "Bumper", PartId=2 });
+
+foreach (Part part in lst)
+{
+    Console.WriteLine(part.PartName.ToString());
+    Console.WriteLine(part.PartId.ToString());
+}
+
+
+
+
 
 
 
